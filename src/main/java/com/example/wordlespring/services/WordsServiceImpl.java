13f185625382afd617d2728d1042483a4a520692
John Doe <barstyle@mail.ru>
2025-10-02 22:47:17 +0300

    fix

--- src/main/java/com/example/wordlespring/services/WordsServiceImpl.java
+++ src/main/java/com/example/wordlespring/services/WordsServiceImpl.java
@@ -2,7 +2,6 @@ package com.example.wordlespring.services;
 
 import com.example.wordlespring.repositories.WordsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.SessionScope;
 
